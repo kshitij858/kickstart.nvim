aef29d51a1b7abff741a1c8e7d3813fa770d768c
Kshitij Sharma <kshitij.sharma@uipath.com>
2025-10-19 23:10:13 +0530

    added persistent plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,44 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'folke/persistence.nvim',
+  event = 'BufReadPre', -- this will only start session saving when an actual file was opened
+  opts = {
+    -- add any custom options here
+  },
+  keys = {
+    -- load the session for the current directory
+    {
+      '<leader>qs',
+      function()
+        require('persistence').load()
+      end,
+      desc = 'Persistence: Load session for current directory',
+    },
+    -- select a session to load
+    {
+      '<leader>qS',
+      function()
+        require('persistence').select()
+      end,
+      desc = 'Persistence: Select session to load',
+    },
+    -- load the last session
+    {
+      '<leader>ql',
+      function()
+        require('persistence').load { last = true }
+      end,
+      desc = 'Persistence: Load last session',
+    },
+    -- stop Persistence => session won't be saved on exit
+    {
+      '<leader>qd',
+      function()
+        require('persistence').stop()
+      end,
+      desc = 'Persistence: Stop session saving',
+    },
+  },
+}
